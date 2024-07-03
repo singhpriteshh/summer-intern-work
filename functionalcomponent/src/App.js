e1dbad1d210d3de3af18490cec346bffde40e909
Singh Pritesh <singhpritesh7599@gmail.com>
2024-07-03 18:06:17 +0530

    added react app

---
+++ functionalcomponent/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1>Hello World!</h1>
+  );
+}
+
+function First() {
+  return(
+    <h1>hehheeheheheheheh</h1>
+  );
+}
+
+function Second(){
+  return(
+    <h1>hahahahahahahahahahahaha</h1>
+  );
+}
+
+export default App;
+export {First,Second};
