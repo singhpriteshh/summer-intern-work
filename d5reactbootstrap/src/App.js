246b9f5d5e62df5b3be0045acbd4c99c344aba6f
Singh Pritesh <singhpritesh7599@gmail.com>
2024-07-05 10:20:59 +0530

    update

---
+++ d5reactbootstrap/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Table } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Button variant='primary'>hello</Button>
+  );
+}
+
+
+function Tble(){
+  return(
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default App;
+export {Tble};
